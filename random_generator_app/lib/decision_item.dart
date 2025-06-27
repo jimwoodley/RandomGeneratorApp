c16ecb4f424012b000d9c140b0f88f2191266541
James <linleyop2@gmail.com>
2025-06-27 19:45:41 +0100

    test 2

--- random_generator_app/lib/decision_item.dart
+++ random_generator_app/lib/decision_item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 // ignore: must_be_immutable
-// temp
 class DecisionItem extends StatelessWidget {
   final String optionText;
   Function(BuildContext)? deleteFunction;
